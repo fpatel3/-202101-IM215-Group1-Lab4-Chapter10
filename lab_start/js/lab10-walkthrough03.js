0586988586306591ca0bbea2b3f69efeb2724012
fpatel3 <forampatel3010@gmail.com>
2021-02-08 11:00:21 -0500

    deleted files not needed
    exercise 9 and 10

--- lab_start/js/lab10-walkthrough03.js
+++ lab_start/js/lab10-walkthrough03.js
@@ -4,3 +4,5 @@
 //$('form	:nth-child(4n)').css('background-color',	'yellow'); // it changed the background colour of all even text areas.
 
 $('label:contains("word")').css("color",	"black");// it changed the text of the labels wich had word in their name.
+
+
