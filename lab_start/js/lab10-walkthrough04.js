ef2e57fc976cc734d9b6af26109204a6bae97fad
fpatel3 <forampatel3010@gmail.com>
2021-02-08 11:07:50 -0500

    exercize 4,5,6
    done by vaidehi

--- lab_start/js/lab10-walkthrough04.js
+++ lab_start/js/lab10-walkthrough04.js
@@ -1,2 +1,30 @@
 /* put your event code here */
+$(function () {
 
+	//	chaining	handlers
+
+	$(".panel")
+
+		.on("mousemove", function (e) {
+
+			$("#message").html("x=" + e.pageX + "	y=" + e.pageY);
+
+		})
+
+		.on("mouseleave", function (e) {
+
+			$("#message").html("goodbye!");
+
+		})
+
+		.on("click", function () {
+
+			$("#message").html("stopped	move	reporting");
+
+			//$("#message").html("You	clicked	in	the	panel");
+
+			$(".panel").off("mousemove");
+
+		});
+
+});	 //done by vaidehi 
